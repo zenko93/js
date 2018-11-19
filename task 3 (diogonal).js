b599bdb382e9e28cd86e3602f24b23aa0ec54d9e
HPp <kkk>
2018-11-19 10:12:25 +0300

    diogonal

--- task 3 (diogonal).js
+++ task 3 (diogonal).js
@@ -1,11 +1,9 @@
 var mas = [];
-var k = 0;
 var n;
 for (let i = 0; i < 5; i++) {
     mas[i]=[];
     for (let j = 0; j < 5; j++) {
-        k = -i;
-        n = k + (j+1);
+        n = -i + (j+1);
         if (n <= 0){
             n = 0;
         }
