50128be6cbeb3bb36fbc8f905a8942175ac10c96
HPp <kkk>
2018-11-19 09:20:45 +0300

    task 1

--- test.js
+++ test.js
@@ -3,11 +3,8 @@ var k =  0
 for (let i = 0; i < 5; i++) {
     mas[i]= [];
     for (let j = 0; j < 5; j++) {
-        k = k + 1;
-        mas[i][j] = k;
+        mas[i][j] = j + 1;
     }
-
 }
-
 console.log(mas);
 
