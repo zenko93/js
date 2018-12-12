564c7688266ef1601810581552fb140363002ef0
HPp <kkk>
2018-12-12 10:04:34 +0300

    unshift

--- addFirst.js
+++ addFirst.js
@@ -1,13 +1,11 @@
 class array {
     constructor(){
         this.i = 0;
-        this.p = 0;
         this.a = [];
-        this.t = 10;
     }
     addFirst(znach){
         this.a[this.i] = znach;
-        this.i++;
+        this.i ++;
     if ( this.a.length > 1 ) {
         znach = this.a[this.a.length - 1];
         for (let i = this.a.length - 1; i > 0; --i) this.a[i] = this.a[i - 1];
@@ -22,7 +20,7 @@ massiv.addFirst(55)
 massiv.addFirst(33)
 massiv.addFirst(1)
 massiv.addFirst(-2)
-
+massiv.addFirst(-6)
 
 console.log(massiv.a)
 
